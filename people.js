12ce20e91818fb249a6ce36c66e7cdc7b135f9d4
inonpeled <inon.peled@gmail.com>
2020-01-30 19:17:12 +0800

    WIP people

---
+++ people.js
@@ -0,0 +1,173 @@
+/* Template
+personIdentifier: {
+        given: '',
+        middle: '',
+        last: '',
+        image: '',
+    },
+*/
+
+const mlsmPeople = {
+    motamediN: {
+        given: 'Nassim',
+        middle: '',
+        last: 'Motamedi',
+        image: '',
+    },
+    yafeiH: {
+        given: 'Han',
+        middle: '',
+        last: 'Yafei',
+        image: '',
+    },
+    dastjerdiA: {
+        given: 'Aliasghar',
+        middle: 'Mehdizadeh',
+        last: 'Dastjerdi',
+        image: '',
+    },
+    markouI: {
+        given: 'Ioulia',
+        middle: '',
+        last: 'Markou',
+        image: '',
+    },
+    romaniniD: {
+        given: 'Daniele',
+        middle: '',
+        last: 'Romanini',
+        image: '',
+    },
+    balleS: {
+        given: 'Sebastian',
+        middle: '',
+        last: 'Balle',
+        image: '',
+    },
+    larsenR: {
+        given: 'Rune',
+        middle: '',
+        last: 'Larsen',
+        image: '',
+    },
+    eckertJ: {
+        given: 'Johannes',
+        middle: 'Benjamin',
+        last: 'Eckert',
+        image: '',
+    },
+    dominguezF: {
+        given: 'Francisco',
+        middle: 'Jose Perez',
+        last: 'Dominguez',
+        image: '',
+    },
+    baburajanV: {
+        given: 'Vishnu',
+        middle: '',
+        last: 'Baburajan',
+        image: '',
+    },
+    martinsA: {
+        given: 'Ana',
+        middle: '',
+        last: 'Martins',
+        image: '',
+    },
+    rodasD: {
+        given: 'David',
+        middle: '',
+        last: 'Rodas',
+        image: '',
+    },
+    antunesF: {
+        given: 'Francisco',
+        middle: '',
+        last: 'Antunes',
+        image: '',
+    },
+    yfantisL: {
+        given: 'Lampros',
+        middle: '',
+        last: 'Yfantis',
+        image: '',
+    },
+    sfeirG: {
+        given: 'Georges',
+        middle: '',
+        last: 'Sfeir',
+        image: '',
+    },
+    monteiroM: {
+        given: 'Mayara',
+        middle: '',
+        last: 'Monteiro',
+        image: '',
+    },
+    renmingL: {
+        given: 'Liu',
+        middle: '',
+        last: 'Renming',
+        image: '',
+    },
+    arkoudiI: {
+        given: 'Ioanna',
+        middle: '',
+        last: 'Arkoudi',
+        image: '',
+    },
+    gammeliD: {
+        given: 'Daniele',
+        middle: '',
+        last: 'Gammeli',
+        image: '',
+    },
+    petersenN: {
+        given: 'Niklas',
+        middle: 'Christopher',
+        last: 'Petersen',
+        image: '',
+    },
+    dongwooL: {
+        given: 'Lee',
+        last: 'Dongwoo',
+        image: '',
+    },
+    kosticB: {
+        given: 'Bojan',
+        last: 'Kostic',
+        image: '',
+    },
+    borysovS: {
+        given: 'Stanislav',
+        middle: 'S',
+        last: 'Borysov',
+        image: '',
+    },
+    azevedoCL: {
+        given: 'Carlos',
+        last: 'Lima Azevedo',
+        image: '',
+    },
+    camaraFC: {
+        given: 'Francisco',
+        middle: 'Cam\{^a}ra',
+        last: 'Pereira',
+        image: '',
+    },
+    rodriguesF: {
+        given: 'Filipe',
+        last: 'Rodrigues',
+        image: '',
+    },
+    henricksonK: {
+        given: 'Kristian',
+        last: 'Henrickson',
+        image: '',
+    },
+    peledI: {
+        given: 'Inon',
+        last: 'Peled',
+        image: '',
+    },
+};
